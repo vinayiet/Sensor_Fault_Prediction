341af5cc89f62b39b55b0ee92f4baabe37b986ad
Vinay Sharma <vinayiet435@gmail.com>
2025-03-28 01:07:27 +0530

    added constants into the project which are going to be used

---
+++ src/constant/__init__.py
@@ -0,0 +1,9 @@
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "wafers"
+MONGO_COLLECTION_NAME = "waferFault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL= ""
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
